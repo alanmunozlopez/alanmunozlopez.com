e55766f0a8b92fb1e60b6499e0a195cd1ca3654c
alanmunozlopez <me@alanmunozlopez.com>
2022-10-27 12:36:21 -0300

    am - updated to worksitelabs end

--- src/pages/home/sections/Description.js
+++ src/pages/home/sections/Description.js
@@ -19,7 +19,7 @@ const Description = () => (
           About
         </h2>
         <p className="color--gray">
-        During these last six years I have been working as a developer mostly focussed on mobile technologies and Javascript, one of my expertises is iOS (using Swift language), Android development as well, React Native for both and Frontend with libraries like Angular, React or Vue. Currently I am working for Globant Chile, Developing a new proyect with scrum methodology for HBO. Previously I have been working as a Javascript full-stack developer for Coopeuch, Lead Developer for Ibex Technologies and mobile developer for Santander Bank Chile. And thanks for read :) 
+        During these last eight years I have been working as a developer mostly focussed on mobile technologies and Javascript, one of my expertises is iOS (using Swift language), Android development as well, React Native for both and Frontend with libraries like Angular, React or Vue. Currently I am working for Globant Chile, Developing a new proyect with scrum methodology for HBO. Previously I have been working as a Javascript full-stack developer for Coopeuch, Lead Developer for Ibex Technologies and mobile developer for Santander Bank Chile. And thanks for read :)
         </p>
       </div>
       <div className="Description-social-networks">
--- src/pages/home/sections/Projects.js
+++ src/pages/home/sections/Projects.js
@@ -4,8 +4,12 @@ import './Projects.css';
 
 import Project from '../components/Project';
 
-import hboGlobantLogo from '../../../assets/images/hboGlobantLogo.jpg';
-import latamGlobantLogo from '../../../assets/images/latamGlobantLogo.jpg'
+import worksitelabsLogo from '../../../assets/images/worksitelabs.png';
+import khealthLogo from '../../../assets/images/khealth.png';
+import unitedLogo from '../../../assets/images/united.png';
+import rockwellLogo from '../../../assets/images/rockwell.png';
+import hboLogo from '../../../assets/images/hbo.png';
+import latamLogo from '../../../assets/images/latam.png'
 import coopeuchLogo from '../../../assets/images/coopeuch.jpg';
 import cutsmodeImage from '../../../assets/images/cutsmode.png';
 import plantsssLogo from '../../../assets/images/plantsss.png';
@@ -17,14 +21,34 @@ const Projects = () => (
     <h3> Projects </h3>
     <div className='Projects-cards'>
       <Project
+          title='Worksitelabs / BairesDev'
+          legend='I have worked on the main appointment App for w Worksitelabs. We created a new and better version of this App to reserve hours of medical samples.  I led the team to create this React App. I managed the dev team, the QA team, and the DevOps team to create a perfect application using functional components, hooks, and the implementation of "xState" a very good library to manage the different states of the app.  I created the GitHub actions for continuous integration and I support the QA team to create the Cypress end-to-end testing project.  I advised the client I to create and integrate a good architecture model for the project (FrontEnd, BackEnd, Hosting, and DB).  The main technologies used: React, xState, React Router Dom, MUI, StyledComponents, GitHub Actions, Cypress, NodeJS.'
+          image={worksitelabsLogo}
+      />
+      <Project
+          title='K Health / BairesDev'
+          legend='I have worked on the main App Mobile for Android and iOS. This app generates medical diagnostics based on AI. I worked on adding new features like Geolocation to select Drugstores and go to get drugs, registration, and payment process to chat with a Doctor, Fixing bugs, and more.  In the last stage, we started to create a website with a new flow to migrate a part of the app to the browser.  The main technologies used: React Native, React, Hooks, Python, MongoDB, and StyledComponents.'
+          image={khealthLogo}
+      />
+      <Project
+          title='United Airlines/ Globant'
+          legend='I have worked on the cart shopping App. I worked on maintaining, fixing bugs, and adding new features for the process of selecting seats, reservations, and initiating payment.  The main technologies I have used were: React, Redux, CSS, Hooks, a11y, and i18n (languages).'
+          image={unitedLogo}
+      />
+      <Project
+          title='Rockwell Automation / Globant'
+          legend='I have worked on IDE based on JS and powered by Electron. Techs: Javascript, Kotlin.'
+          image={rockwellLogo}
+      />
+      <Project
         title='HBO / Globant'
         legend='Currently I’m working on a big project for HBO. The company has an internal system to administrate the assets for the rest of the platforms where HBO services are present. The main technologies are: AngularJS, Angular2+, Bootstrap, Sass and Karma.'
-        image={hboGlobantLogo}
+        image={hboLogo}
       />
       <Project
         title='Latam Airlines / Globant'
         legend='I have worked in a compensation system for LATAM airlines, which allows customers to get compensations directly to their bank accounts. This involve interface with some internals Back End systems to do electronics transfers and also a Front End interface for the end users. The Back End is implemented in Node JS / Express And the Front End is ReactJS / Redux.'
-        image={latamGlobantLogo}
+        image={latamLogo}
       />
       <Project
         title='Coopeuch'
